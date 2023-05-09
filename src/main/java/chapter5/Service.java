49486aed2b3d00d30daadd640072345ee0fb447c
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-05-09 09:39:10 +0200

    remove main method

--- src/main/java/chapter5/Service.java
+++ src/main/java/chapter5/Service.java
@@ -1,8 +1,6 @@
 package chapter5;
 
-import com.google.inject.Guice;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 
 public class Service {
     private Interface anInterface;
@@ -14,14 +12,5 @@ public class Service {
 
     public Interface anInterface() {
         return anInterface;
-    }
-
-    public static void main(String[] args) {
-        Injector injector = Guice.createInjector(binder ->
-                binder.bind(Interface.class).to(InterfaceImpl.class));
-
-        Interface instance = injector.getInstance(Interface.class);
-
-        System.out.println(instance.getClass().getSimpleName());
     }
 }
