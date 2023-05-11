71e1ef0c05c8b897300dc864e7c3f3ad6e2f75bf
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-05-11 08:55:10 +0200

    remove @Inject

--- src/main/java/chapter4/BuyerService.java
+++ src/main/java/chapter4/BuyerService.java
@@ -1,11 +1,8 @@
 package chapter4;
 
-import com.google.inject.Inject;
-
 public class BuyerService {
     private final String name;
 
-    @Inject
     public BuyerService(String name) {
         this.name = name;
     }
