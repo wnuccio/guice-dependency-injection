f22a7128615e1208c041482be761bec6c70958dd
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-05-11 09:09:19 +0200

    remove bindings

--- src/main/java/chapter10/OrderModule.java
+++ src/main/java/chapter10/OrderModule.java
@@ -5,7 +5,5 @@ import com.google.inject.AbstractModule;
 public class OrderModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(PriceService.class).toInstance(new PriceService());
-        bind(ProductService.class).to(ProductServiceImpl.class);
     }
 }
